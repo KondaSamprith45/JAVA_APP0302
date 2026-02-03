d959ee1464e07b441799cc9658d58f822be57571
Samprith Reddy <samprithkonda@gmail.com>
2026-02-03 22:54:56 +0530

    Reverse of a number

---
+++ reverse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class reverse{
+	public static void main(String[] args){
+	
+	Scanner scanner= new Scanner(System.in);
+	
+	int n=scanner.nextInt();
+	
+	int ans=0;
+	
+	while(n>0){
+	
+		 int rem=n%10;
+		 
+		 n/=10;
+		 
+		 ans=ans*10+rem;
+		 
+		 
+		 
+		 }
+		 
+		System.out.println(ans);
+		 }
+		}
